82e5c6dc7787afe98c3192dc4309064620db87be
Louis - <66411027+LuiAr@users.noreply.github.com>
2022-12-07 16:33:33 +0000

    added commands handling

--- index.js
+++ index.js
@@ -23,11 +23,50 @@ const OverwatchSRHistoric = new Schema({
 });
 
 // DISCORD JS
-const { Client, GatewayIntentBits , EmbedBuilder , PermissionsBitField , Permissions, MessageEmbed, ActionRowBuilder, ButtonBuilder, ButtonStyle ,ModalBuilder,TextInputBuilder, TextInputStyle , AttachmentBuilder  } = require('discord.js');
+
+// For commands handling
+const fs = require('node:fs');
+const path = require('node:path');
+
+const { Client, Collection, GatewayIntentBits , Events , EmbedBuilder , PermissionsBitField , Permissions, MessageEmbed, ActionRowBuilder, ButtonBuilder, ButtonStyle ,ModalBuilder,TextInputBuilder, TextInputStyle , AttachmentBuilder  } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds , GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 const prefix = '!';
 
+// For commands
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 
 client.on('ready', () => {
     // get the hour, min and sec
@@ -38,6 +77,9 @@ client.on('ready', () => {
     // log the ready message
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`Ready at ${hour}:${min}:${sec}`);
+
+	// Presence message
+	// client.user.setActivity({ type: ActivityType.Watching, name: "!help for all commands" });
 });
 
 client.on("messageCreate" , (message) => {
